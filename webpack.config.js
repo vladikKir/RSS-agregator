72e885bafa68270aed385ba38d2ec7e756db151c
vladikKir <vlad@Vlad.localdomain>
2022-10-20 02:07:27 +0300

    Add public dir, fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -13,58 +13,59 @@ const stylesHandler = MiniCssExtractPlugin.loader;
 
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		open: true,
+		host: 'localhost',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+		}),
 
-        new MiniCssExtractPlugin(),
+		new MiniCssExtractPlugin(),
+    
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-              test: /\.scss$/,
-              use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/i,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: [stylesHandler,'css-loader'],
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset',
+			},
 
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		],
+	},
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
+	if (isProduction) {
+		config.mode = 'production';
         
         
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+		config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
         
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+	} else {
+		config.mode = 'development';
+	}
+	return config;
 };
